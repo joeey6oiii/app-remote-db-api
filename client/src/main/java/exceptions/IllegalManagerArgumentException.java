924a8b27ad32f0c58b23143e34f5869e8e56f061
Alexey Rumyantsev <115720596+kseikoff@users.noreply.github.com>
2023-04-24 14:29:46 +0300

    Delete IllegalManagerArgumentException.java

--- client/src/main/java/exceptions/IllegalManagerArgumentException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class IllegalManagerArgumentException extends Exception {
-    public IllegalManagerArgumentException(String message) {
-        super(message);
-    }
-}
